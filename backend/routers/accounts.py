bd06e40e4a12774395009de988bc6f4d72ae8b1e
Gogyoq <aidan.kooiman@gmail.com>
2026-01-11 17:20:41 -0500

    Quick fix

--- backend/routers/accounts.py
+++ backend/routers/accounts.py
@@ -120,7 +120,7 @@ def authenticate_user(username: str, password: str):
     
     return user
 
-def create_access_token(data: dict, expires_delta: timedelta or None = None):
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now() + expires_delta
